ceea689ff4efa20032e4b14f6ab2b90ac47ae8e1
mertceyhan <c.mertceyhn@gmail.com>
2021-10-26 10:44:55 +0200

    Update for splash screen migration

--- app/src/main/java/com/mertceyhan/bitcoinmarket/features/splash/ui/SplashFragment.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/features/splash/ui/SplashFragment.kt
@@ -1,33 +1,79 @@
 package com.mertceyhan.bitcoinmarket.features.splash.ui
 
-import android.os.Bundle
-import android.view.View
-import androidx.lifecycle.lifecycleScope
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.mertceyhan.R
-import com.mertceyhan.bitcoinmarket.core.ui.BaseFragment
-import com.mertceyhan.databinding.FragmentSplashBinding
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.mertceyhan.bitcoinmarket.core.ui.BaseComposeFragment
+import com.mertceyhan.bitcoinmarket.core.ui.theme.BitcoinMarketTheme
 
-class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+class SplashFragment : BaseComposeFragment() {
 
-    override fun getLayoutId(): Int = R.layout.fragment_splash
+    @Composable
+    override fun Content() {
+        SplashScreen(navigationController = findNavController())
+    }
+}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+@Composable
+fun SplashScreen(
+    navigationController: NavController? = null,
+    alphaAnimationTargetValue: Float = 0f,
+    alphaAnimationDurationMillis: Int = 1000
+) {
+    BitcoinMarketTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            var targetValue by remember { mutableStateOf(alphaAnimationTargetValue) }
 
-        lifecycleScope.launch {
-            delay(SPLASH_NAVIGATION_DELAY)
-            navigateToMarketFragment()
+            val alpha: Float by animateFloatAsState(
+                targetValue = targetValue,
+                animationSpec = tween(alphaAnimationDurationMillis),
+                finishedListener = {
+                    navigationController?.navigate(
+                        SplashFragmentDirections.actionSplashFragmentToBitcoinMarketFragment()
+                    )
+                }
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_app),
+                contentDescription = null,
+                modifier = Modifier.alpha(alpha)
+            )
+
+            LaunchedEffect(Unit) {
+                targetValue = 1f
+            }
         }
     }
+}
 
-    private fun navigateToMarketFragment() {
-        findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToBitcoinMarketFragment())
-    }
+@Preview
+@Composable
+private fun PreviewSplashScreen() {
+    SplashScreen(alphaAnimationTargetValue = 1f)
+}
 
-    companion object {
-        private const val SPLASH_NAVIGATION_DELAY: Long = 1000
-    }
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewSplashScreenDark() {
+    SplashScreen(alphaAnimationTargetValue = 1f)
 }
