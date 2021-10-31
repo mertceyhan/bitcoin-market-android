47d002ece3b6c0ef5161b581cc1f05ab5c608943
Furkan Boran <boranfrkn@gmail.com>
2021-10-31 18:17:20 +0300

    ui navigation to compose navigation

---
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/navigation/BitcoinMarketApplication.kt
@@ -0,0 +1,33 @@
+package com.mertceyhan.bitcoinmarket.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mertceyhan.bitcoinmarket.features.market.ui.MarketScreen
+import com.mertceyhan.bitcoinmarket.features.splash.ui.SplashScreen
+
+
+@Composable
+fun BitcoinMarketApplication(){
+    val navigation = rememberNavController()
+
+    NavHost(
+        navController = navigation,
+        startDestination = Screen.Splash.route
+    ){
+        composable(
+            route = Screen.Splash.route
+        ){
+            SplashScreen(
+                navigationController = navigation
+            )
+        }
+
+        composable(
+            route = Screen.Market.route
+        ){
+            MarketScreen()
+        }
+    }
+}
