e1ffa6808c18e0e0fe7756312a09cc00f604b087
Mustafa YIGIT <mustafa.yt65@gmail.com>
2021-11-01 15:56:29 +0300

    Implement SwipeToRefresh to MarketScreen

--- app/src/main/java/com/mertceyhan/bitcoinmarket/features/market/ui/MarketScreen.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/features/market/ui/MarketScreen.kt
@@ -3,6 +3,8 @@ package com.mertceyhan.bitcoinmarket.features.market.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,6 +15,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.mertceyhan.R
 import com.mertceyhan.bitcoinmarket.components.*
 import com.mertceyhan.bitcoinmarket.core.ui.UiState
@@ -25,6 +29,9 @@ import com.mertceyhan.bitcoinmarket.features.loading.LoadingScreen
 fun MarketScreen(marketViewModel: MarketViewModel = hiltViewModel()) {
 
     val uiState by marketViewModel.getUiState()
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = uiState is UiState.Loading)
+
+    val scrollState = rememberScrollState()
 
     when (uiState) {
         is UiState.Success -> {
@@ -32,51 +39,61 @@ fun MarketScreen(marketViewModel: MarketViewModel = hiltViewModel()) {
             val viewState = (uiState as UiState.Success<MarketScreenViewState>).data
 
             Surface {
-                Column {
-                    PriceHeader(
+                SwipeRefresh(
+                    state = swipeRefreshState,
+                    onRefresh = {
+                        marketViewModel.getMarketInformation(viewState.getTimeRange())
+                    },
+                ) {
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp, top = 16.dp, end = 16.dp),
-                        currency = stringResource(id = R.string.bitcoin_btc),
-                        price = viewState.marketInformation.currentPrice,
-                        changeRate = viewState.marketInformation.changeRate,
-                        isChangeRatePositive = viewState.isChangeStatusPositive()
-                    )
+                            .verticalScroll(scrollState)
+                    ) {
+                        PriceHeader(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+                            currency = stringResource(id = R.string.bitcoin_btc),
+                            price = viewState.marketInformation.currentPrice,
+                            changeRate = viewState.marketInformation.changeRate,
+                            isChangeRatePositive = viewState.isChangeStatusPositive()
+                        )
 
-                    TimeRangePicker(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp, top = 16.dp, end = 16.dp),
-                        selectedTimeRange = viewState.getTimeRange()
-                    ) { timeRange ->
-                        marketViewModel.getMarketInformation(timeRange)
+                        TimeRangePicker(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+                            selectedTimeRange = viewState.getTimeRange()
+                        ) { timeRange ->
+                            marketViewModel.getMarketInformation(timeRange)
+                        }
+
+                        Chart(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            lineDataSet = viewState.getLineDataSet(LocalContext.current),
+                        )
+
+                        Price(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+                            openPrice = viewState.marketInformation.openPrice,
+                            closePrice = viewState.marketInformation.closePrice,
+                            highPrice = viewState.marketInformation.highPrice,
+                            lowPrice = viewState.marketInformation.lowPrice,
+                            averagePrice = viewState.marketInformation.averagePrice,
+                            changePrice = viewState.marketInformation.changePrice
+                        )
+
+                        AboutChart(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 24.dp, end = 16.dp),
+                            aboutChart = viewState.marketInformation.aboutChart
+                        )
                     }
-
-                    Chart(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 8.dp),
-                        lineDataSet = viewState.getLineDataSet(LocalContext.current),
-                    )
-
-                    Price(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp, top = 16.dp, end = 16.dp),
-                        openPrice = viewState.marketInformation.openPrice,
-                        closePrice = viewState.marketInformation.closePrice,
-                        highPrice = viewState.marketInformation.highPrice,
-                        lowPrice = viewState.marketInformation.lowPrice,
-                        averagePrice = viewState.marketInformation.averagePrice,
-                        changePrice = viewState.marketInformation.changePrice
-                    )
-
-                    AboutChart(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp, top = 24.dp, end = 16.dp),
-                        aboutChart = viewState.marketInformation.aboutChart
-                    )
                 }
             }
         }
