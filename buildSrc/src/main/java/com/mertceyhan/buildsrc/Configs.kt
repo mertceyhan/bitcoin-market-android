ebb639fc8772270fcd84e7c9df014175f5146daa
Cafer Mert Ceyhan <c.mertceyhn@gmail.com>
2021-11-24 17:01:07 +0100

    Release v2.0.1

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -7,7 +7,7 @@ object Configs {
     const val applicationId = "com.mertceyhan.bitcoinmarket"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 5
-    const val versionName = "2.0.0"
+    const val versionCode = 6
+    const val versionName = "2.0.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
