6663985f42cb79ffc19b1dd9dc24fbbc09e5e135
enofeb <eneszor95@gmail.com>
2021-12-23 19:50:55 +0300

    Unused imports were removed

--- app/src/main/java/com/mertceyhan/bitcoinmarket/widget/MarketWidget.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/widget/MarketWidget.kt
@@ -1,7 +1,6 @@
 package com.mertceyhan.bitcoinmarket.widget
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.*
 import androidx.glance.appwidget.GlanceAppWidget
@@ -11,6 +10,8 @@ import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.mertceyhan.R
+import com.mertceyhan.bitcoinmarket.core.ui.theme.BitcoinMarketTheme
+import com.mertceyhan.bitcoinmarket.utils.extensions.darkModeEnabled
 
 class MarketWidget : GlanceAppWidget() {
 
@@ -19,40 +20,41 @@ class MarketWidget : GlanceAppWidget() {
 
         val context = LocalContext.current
 
-        Column(
-            modifier = GlanceModifier
-                .width(170.dp).height(130.dp)
-                .background(ImageProvider(R.drawable.background_widget))
-                .padding(8.dp)
-        ) {
-            Row(
-                modifier = GlanceModifier.fillMaxWidth()
+        BitcoinMarketTheme(darkTheme = context.darkModeEnabled()) {
+            Column(
+                modifier = GlanceModifier
+                    .width(170.dp).height(130.dp)
+                    .background(ImageProvider(R.drawable.background_widget))
+                    .padding(8.dp)
             ) {
-                Image(
-                    provider = ImageProvider(R.drawable.ic_btc_flat),
-                    contentDescription = "btc"
-                )
                 Row(
-                    horizontalAlignment = Alignment.End,
                     modifier = GlanceModifier.fillMaxWidth()
                 ) {
                     Image(
-                        provider = ImageProvider(R.drawable.ic_refresh),
-                        contentDescription = "refresh"
+                        provider = ImageProvider(R.drawable.ic_btc_flat),
+                        contentDescription = "btc"
                     )
+                    Row(
+                        horizontalAlignment = Alignment.End,
+                        modifier = GlanceModifier.fillMaxWidth()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.ic_refresh),
+                            contentDescription = "refresh"
+                        )
+                    }
                 }
+                Text(
+                    text = context.getString(R.string.bitcoin_label),
+                    modifier = GlanceModifier.fillMaxWidth().padding(top = 8.dp),
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                )
+                Text(
+                    text = "$48.823,08",
+                    modifier = GlanceModifier.fillMaxWidth().padding(top = 8.dp),
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                )
             }
-            Text(
-                text = context.getString(R.string.bitcoin_label),
-                modifier = GlanceModifier.fillMaxWidth().padding(top = 8.dp),
-                style = TextStyle(fontWeight = FontWeight.Bold),
-            )
-            Text(
-                text = "$48.823,08",
-                modifier = GlanceModifier.fillMaxWidth().padding(top = 8.dp),
-                style = TextStyle(fontWeight = FontWeight.Bold),
-            )
         }
-
     }
 }
