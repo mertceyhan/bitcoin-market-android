a6c2c252f33c9eb0f8f32f64f865ca48739c1461
enofeb <eneszor95@gmail.com>
2021-12-26 17:20:34 +0300

    MarketRefreshCallback on run was updated

--- app/src/main/java/com/mertceyhan/bitcoinmarket/widget/callback/MarketRefreshCallback.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/widget/callback/MarketRefreshCallback.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.glance.GlanceId
 import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.action.ActionCallback
+import com.mertceyhan.bitcoinmarket.widget.MarketWidget
 
 class MarketRefreshCallback : ActionCallback {
 
     override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
-
+        MarketWidget().update(context, glanceId)
     }
 }
