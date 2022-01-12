4f86046f7ee20048b09fbcbe4b553421dc5d3ee6
enofeb <eneszor95@gmail.com>
2022-01-12 23:09:08 +0300

    BitcoinWidgetUiState tests were added

--- app/src/main/java/com/mertceyhan/bitcoinmarket/widget/ui/BitcoinWidgetUiState.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/widget/ui/BitcoinWidgetUiState.kt
@@ -1,6 +1,5 @@
 package com.mertceyhan.bitcoinmarket.widget.ui
 
-import androidx.annotation.VisibleForTesting
 import com.mertceyhan.R
 
 class BitcoinWidgetUiState(
@@ -8,14 +7,12 @@ class BitcoinWidgetUiState(
     val changeRate: String,
     val isChangeRatePositive: Boolean
 ) {
-    @VisibleForTesting
     fun getRateBackground() = if (isChangeRatePositive) {
         R.drawable.background_widget_positive_rate
     } else {
         R.drawable.background_widget_negative_rate
     }
 
-    @VisibleForTesting
     fun getRateArrow() = if (isChangeRatePositive) {
         R.drawable.ic_arrow_positive_white
     } else {
