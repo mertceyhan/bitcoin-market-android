f0f545c52ef97efedc5e06c58f2a91687d443382
mertceyhan <c.mertceyhn@gmail.com>
2022-03-20 18:05:15 +0100

    Release v2.0.2

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -7,7 +7,7 @@ object Configs {
     const val applicationId = "com.mertceyhan.bitcoinmarket"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 6
-    const val versionName = "2.0.1"
+    const val versionCode = 7
+    const val versionName = "2.0.2"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
