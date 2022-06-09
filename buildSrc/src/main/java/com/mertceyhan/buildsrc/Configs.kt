cbfdb8563925b7749a35ec9f16962e72a749d019
halilozel1903 <halilozel1903@gmail.com>
2022-06-09 21:32:17 +0300

    Library versions were upgraded.

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -2,11 +2,10 @@
 
 object Configs {
 
-    const val compileSdkVersion = 31
-    const val buildToolsVersion = "30.0.3"
+    const val compileSdkVersion = 32
     const val applicationId = "com.mertceyhan.bitcoinmarket"
     const val minSdkVersion = 21
-    const val targetSdkVersion = 30
+    const val targetSdkVersion = 32
     const val versionCode = 7
     const val versionName = "2.0.2"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
